417a14cfaa33c5d44939f874a162bd56a3ce5a22
Nolan <tranfc911@gmail.com>
2024-04-26 03:20:29 +0700

    LayoutChung

---
+++ APK_QLNhaHang-main/src/POJO/Connect.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POJO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class Connect {
+
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+
+    String strServer = "DESKTOP-MEUMH4V";
+    String strDatabase = "QL_NhaHang";
+    String strUser = "S1";
+    String strPassword = "123";
+
+    public Connect() {
+        ketNoiCSDL();
+    }
+
+    void ketNoiCSDL() {
+        try {
+            try {
+                String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+                Class.forName(driver);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            // Thêm cài đặt SSL vào URL kết nối
+            String connectURL = "jdbc:sqlserver://" + strServer
+                    + ":1433;databaseName=" + strDatabase
+                    + ";user=" + strUser + ";password=" + strPassword
+                    + ";encrypt=true;trustServerCertificate=true;";
+            connection = DriverManager.getConnection(connectURL);
+            if (connection != null) {
+                System.out.println("Kết nối thành công");
+            } else {
+                System.out.println("Kết nối thất bại");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        Connect db = new Connect();
+       
+    }
+}
